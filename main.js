3e7f0f0b7f52d75881767c80e9534165bf9ea0f9
oscar burbano <oscar10181@hotmail.com>
2024-08-28 19:17:03 -0500

    solucion del los ejercicios 4 y 6 del nivel 2

--- main.js
+++ main.js
@@ -24,30 +24,68 @@
 // Modificar té a 'té verde'
 
 
-const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey'];
-let elemets = shoppingCart.indexOf('carne' && 'azùcar');
-const alegico = false
+// const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey'];
+// let elemets = shoppingCart.indexOf('carne' && 'azùcar');
+// const alegico = false
 
 
-if (elemets != -1) {
-    console.log('el elemento carne y azucar existen en el array',shoppingCart);
+// if (elemets != -1) {
+//     console.log('el elemento carne y azucar existen en el array',shoppingCart);
     
-}else{
-    shoppingCart.unshift('carne'); /// Añade 'Carne' al principio de tu carrito de compras si aún no lo has añadido.
-    shoppingCart.push('azucar');   ///// Agregue azúcar al final de su carrito de compras si aún no lo ha agregado
-    const conAgregados = shoppingCart
+// }else{
+//     shoppingCart.unshift('carne'); /// Añade 'Carne' al principio de tu carrito de compras si aún no lo has añadido.
+//     shoppingCart.push('azucar');   ///// Agregue azúcar al final de su carrito de compras si aún no lo ha agregado
+//     const conAgregados = shoppingCart
     
-    let miel = conAgregados.indexOf('Honey');
+//     let miel = conAgregados.indexOf('Honey');
 
-    if (alegico == true) {
+//     if (alegico == true) {
         
-        conAgregados.splice(miel ,1);
-    }
-     let modi = conAgregados.indexOf('Tea')
-     if (modi !== -1) {
-        conAgregados[modi] = 'Te verde'
-     }
+//         conAgregados.splice(miel ,1);
+//     }
+//      let modi = conAgregados.indexOf('Tea')
+//      if (modi !== -1) {
+//         conAgregados[modi] = 'Te verde'
+//      }
 
 
-    console.log('elemento agrgado',conAgregados);
-}
+//     console.log('elemento agrgado',conAgregados);
+// }
+
+/////////////////////////////////////////////////////////
+
+// En la matriz de países, verifique si existe "Etiopía" en la matriz. Si existe, imprima "Rusia". Si no existe, agréguelo a la lista de países.
+
+// const countries = [
+//     'Albania',
+//     'Bolivia',
+//     'Canada',
+//     'Denmark',
+//     'Ethiopia',
+//     'Finland',
+//     'Germany',
+//     'Hungary',
+//     'Ireland',
+//     'Japan',
+//     'Kenya'
+//   ]
+
+//   const exisPais = countries.indexOf('Rusia');
+  
+
+//   if (exisPais !== -1) {
+//     console.log('este pais ya existe')
+//   }else{
+//     countries.push('Rusia');
+//     console.log(countries);
+//   }
+
+
+////// Concatene las siguientes dos variables y almacénelas en una variable fullStack.
+
+const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+const backEnd = ['Node', 'Express', 'MongoDB']
+
+const fullStack = frontEnd.concat(backEnd);
+
+console.log(fullStack)
