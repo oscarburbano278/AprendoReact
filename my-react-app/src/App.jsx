151d4a4208c9cdb90cc9b7611b5f8398220679c7
oscar burbano <oscar10181@hotmail.com>
2024-11-14 15:58:52 -0500

    configuracion (borrado)  de entorno para empezar un proyecto nuevo

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,36 +1,12 @@
-//import Boton from './Componente/Boton'
-
-import Header from './Componente/Header';
-import Aside from './Componente/Aside';
 import './App.css'
-import Boton from './Componente/Boton';
+
 
 function App() {
 
   return (
     <div className="App">
 
-      <div className="containerHeader">      
-        <Header 
-        title = 'SUSCRIBETE'
-        description='incribete con tu direccion de email'
-        />
-        
-      </div>
       
-
-      <div className="contenedorCuerpo">
-          <Aside type='text' placehold='firsName'/>
-          <Aside type='text' placehold='lastName'/>
-          <Aside type='email' placehold='Email'/>
-      </div>
-
-      <div className="containerBoton">
-        <Boton>
-          Subscribirse
-        </Boton>
-        
-      </div>
 
      
 
